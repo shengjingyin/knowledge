59579bfb6450dc0d1fac6588f9ad4e29732a4784
阿银 <739178270@qq.com>
2022-10-26 15:08:20 +0800

    调整结构

--- src/entry.ts
+++ src/entry.ts
@@ -1,8 +1,8 @@
 import { App } from "vue";
 
 import SButton from "./button";
-import SFCButton from "./SFCButton.vue";
-import JSXButton from "./JSXButton";
+import SFCButton from "./button/SFCButton.vue";
+import JSXButton from "./button/JSXButton";
 
 // 导出单独组件
 export { SButton, SFCButton, JSXButton };
@@ -10,9 +10,9 @@ export { SButton, SFCButton, JSXButton };
 // 编写一个插件，实现install方法
 
 export default {
-  install(app: App): void {
-    app.component(SButton.name, SButton);
-    app.component(SFCButton.name, SFCButton);
-    app.component(JSXButton.name, JSXButton);
-  },
+	install(app: App): void {
+		app.component(SButton.name, SButton);
+		app.component(SFCButton.name, SFCButton);
+		app.component(JSXButton.name, JSXButton);
+	},
 };
